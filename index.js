f4eaf0db56e8cff9d8ed286c083c948a47fabfc3
John Grib <johngrib@woowahan.com>
2016-08-07 08:42:39 +0900

    added date checker

--- index.js
+++ index.js
@@ -6,31 +6,43 @@ const program = require('commander');
 const getJSON = require('get-json');
 const bob_url = 'https://spreadsheets.google.com/feeds/list/1GqW4dY5DSUZ4Qkg7NnbKBrQnaw9goNooarjb1IgqYac/od6/public/values?alt=json';
 
-program
-  .version('0.0.1')
-  .usage('[options]')
-  .option('-n, --next <number>', 'An integer argument', parseInt)
-  .option('-d, --date <YYYY.MM.dd>', 'date formatted string argument', parseInt)
-  .parse(process.argv);
+(function(){
+    program
+      .version('0.0.1')
+      .usage('[options]')
+      .option('-n, --next <number>', 'An integer argument', parseInt)
+      .option('-d, --date <YYYY.MM.dd>', 'date formatted string argument', (v) => new Date(v))
+      .parse(process.argv);
 
-if(typeof program.next === 'undefined'){
-    program.next = 0;
-}
+    program.next = program.next || 0;
+    program.date = program.date || new Date();
 
-getJSON(bob_url, main);
+    getJSON(bob_url, main);
+})();
 
 function main(error, json) {
 
-    const today = new Date().setHours(0,0,0,0);
-    const tomorrow = new Date(today).setHours(24,0,0,0);
+    const today = program.date;
     const list = json.feed.entry;
 
     // 재귀 함수에 리미터를 부착하여 오버플로우를 방지한다.
     const search = add_limiter_on_function(binary_search_slicer, 100);
+    const date_checker = get_date_checker(today);
 
-    const result = search({ head: [], body: list, tail: []}, today, tomorrow);
+    const result = search.bind
+        ({}, date_checker)
+        ({ head: [], body: list, tail: []});
 
-    print_result(result.body);
+    const result_list = (function(next){
+        if(next > 0)
+            return result.body.concat(result.tail.slice(0, next));
+        if(next < 0)
+            return result.head.slice(result.head.length + next).concat(result.body);
+        else
+            return result.body;
+    })(program.next);
+
+    print_result(result_list);
 
     return 0;
 }
@@ -62,9 +74,22 @@ function add_limiter_on_function(func, limit) {
     };
 }
 
-function binary_search_slicer(list, today, tomorrow) {
+function get_date_checker(date) {
+    const today = new Date(date).setHours(0,0,0,0);
+    const tomorrow = new Date(today).setHours(24,0,0,0);
 
-    console.log(list.head.length, list.body.length, list.tail.length);
+    return function(check_date){
+        if(check_date < today)
+            return -1;
+        if(check_date >= tomorrow)
+            return 1;
+        else
+            return 0;
+    };
+}
+
+function binary_search_slicer(date_checker, list) {
+
     var body = list.body;
 
     if(list.body.length < 1) {
@@ -72,21 +97,25 @@ function binary_search_slicer(list, today, tomorrow) {
     }
 
     const mid = parseInt(body.length / 2, 10);
-    const time = new Date(body[mid].gsx$date.$t).getTime();
+    const time = new Date(body[mid].gsx$date.$t).setHours(0,0,0,0);
+    const check = date_checker(time);
     let newList = {};
 
-    if(time < today) {
+    if(check < 0) {
         newList.head = list.head.concat(list.body.slice(0, mid+1));
         newList.body = list.body.slice(mid+1);
         newList.tail = list.tail;
-        return binary_search_slicer(newList, today, tomorrow);
-    } else if(time > tomorrow) {
+        return binary_search_slicer(date_checker, newList);
+    } else if(check > 0) {
         newList.head = list.head;
-        newList.body = list.body.slice(0, mid-1);
+        newList.body = list.body.slice(0, mid);
         newList.tail = list.body.slice(mid).concat(list.tail);
-        return binary_search_slicer(newList, today, tomorrow);
+        return binary_search_slicer(date_checker, newList);
     } else {
-        return list;
+        newList.head = list.head.concat(list.body.slice(0, mid));
+        newList.body = list.body.slice(mid, mid+1);
+        newList.tail = list.body.slice(mid+1).concat(list.tail);
+        return newList;
     }
 }
 
